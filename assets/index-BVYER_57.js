dffc60416bcd33cbd2c9584f98290ff3202a8f94
sahilsync07 <sahilsync07@users.noreply.github.com>
2026-07-01 15:20:25 +0000

    deploy: 734391ecf8d90393a7362277750664ad9550af77

---
+++ assets/index-BVYER_57.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B--dAwuL.js","assets/index-gv85PfCA.js","assets/index-DNLnOI1N.css"])))=>i.map(i=>d[i]);
+import{j as p,_ as r}from"./index-gv85PfCA.js";const _=p("App",{web:()=>r(()=>import("./web-B--dAwuL.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as A};
