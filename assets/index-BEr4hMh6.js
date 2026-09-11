735897622cbcc6663ccca3f11f715db5fc058ce7
sahilsync07 <sahilsync07@users.noreply.github.com>
2026-09-11 04:07:15 +0000

    deploy: af60ae5e054d526ea46f1f234f5feb7db28814d4

---
+++ assets/index-BEr4hMh6.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-VrsW374h.js","assets/index--RugvRoI.js","assets/index-ywZUKNJf.css"])))=>i.map(i=>d[i]);
+import{h as p,_ as r}from"./index--RugvRoI.js";const _=p("App",{web:()=>r(()=>import("./web-VrsW374h.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as A};
