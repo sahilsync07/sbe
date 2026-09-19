04c407ae89951c250016015b2b10546600114598
sahilsync07 <sahilsync07@users.noreply.github.com>
2026-09-19 06:11:05 +0000

    deploy: a688f35ee84ba4d92028604aeb6c2c39270959a0

---
+++ assets/index-DAc4bWFv.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BnvX5wOi.js","assets/index-D598D6P-.js","assets/index-Z8Jy0wdG.css"])))=>i.map(i=>d[i]);
+import{h as p,_ as r}from"./index-D598D6P-.js";const _=p("App",{web:()=>r(()=>import("./web-BnvX5wOi.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as A};
